33d06f89c4f672f5a4740041e214d4143c50f6f5
ken0063 <olukotunk@gmail.com>
2023-08-04 02:40:07 +0100

    fix PWA

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "prompt",
+      devOptions: {
+        enabled: true,
+      },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
         name: "Summlytic",
@@ -16,20 +19,29 @@ export default defineConfig({
           "With Summlytic, you can now effortlessly access concise and comprehensive summaries of lengthy articles from various sources",
         theme_color: "#ffffff",
         start_url: "/",
+        scope: "/",
+        orientation: "portrait",
+        id: "summlytic",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon .png",
+            sizes: "1024x1024",
             type: "image/png",
             purpose: "any maskable",
           },
