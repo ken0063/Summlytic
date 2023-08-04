31b32bbcf6aba0593b3c38ddf2492f2620166468
ken0063 <olukotunk@gmail.com>
2023-08-04 02:50:39 +0100

    test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mv0276coefo"
+    "revision": "0.lqig1lu7e4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -24,23 +24,23 @@ export default defineConfig({
         id: "summlytic",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "/pwa/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/pwa/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/apple-touch-icon.png",
+            src: "/pwa/apple-touch-icon.png",
             sizes: "180x180",
             type: "image/png",
             purpose: "apple touch icon",
           },
           {
-            src: "/maskable_icon .png",
+            src: "/pwa/maskable_icon .png",
             sizes: "1024x1024",
             type: "image/png",
             purpose: "any maskable",
