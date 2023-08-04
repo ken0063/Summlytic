a5078df4c106b5778351429fa7627ea00716b4f1
ken0063 <olukotunk@gmail.com>
2023-08-04 03:07:07 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "prompt",
-      devOptions: {
-        enabled: true,
-      },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
         name: "Summlytic",
